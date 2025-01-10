d4716754ff488a52cccbf4a0b77409b2e7d4e3e7
Rifat Sarker <rifatswd@gmail.com>
2025-01-10 20:10:58 +0600

    add theme provider and toggle theme

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,14 +1,20 @@
 import { Link } from "react-router-dom";
+import { ModeToggle } from "../mode-toggle";
 
 export default function Navbar() {
   return (
-    <div className="max-w-6xl mx-auto py-4 px-6 flex items-center space-x-6">
-      <h1 className="font-bold">
-        <span className="text-2xl text-pink-500">Task</span>{" "}
-        <span className="text-2xl">Mate</span>
-      </h1>
+    <nav className="max-w-6xl mx-auto py-4 h-16 px-6 flex items-center gap-3">
+      <div className="flex items-center">
+        <h1 className="font-bold">
+          <span className="text-2xl text-pink-500">Task</span>{" "}
+          <span className="text-2xl">Mate</span>
+        </h1>
+      </div>
       <Link to={"users"}>Go to users</Link>
       <Link to={""}>Go to Task lists</Link>
-    </div>
+      <div className="ml-auto">
+        <ModeToggle />
+      </div>
+    </nav>
   );
 }
