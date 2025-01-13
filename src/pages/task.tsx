bbd2065987d15b3edf98b5ccc34674c8bf4f244d
Rifat Sarker <rifatswd@gmail.com>
2025-01-13 11:41:35 +0600

    add AddTaskModal component and integrate into Task page; update styles and dependencies

--- src/pages/task.tsx
+++ src/pages/task.tsx
@@ -1,3 +1,4 @@
+import { AddTaskModal } from "@/components/module/Tasks/AddTaskModal";
 import TaskCard from "@/components/module/Tasks/TaskCard";
 import { selectTasks } from "@/redux/features/task/taskSlice";
 import { useAppSelector } from "@/redux/hook";
@@ -7,8 +8,9 @@ export default function Task() {
 
   return (
     <div className="max-w-7xl mx-auto px-5 mt-20">
-      <div>
+      <div className="flex justify-between items-center mt-5">
         <h1>Task</h1>
+        <AddTaskModal />
       </div>
       <div className="space-y-5 mt-20">
         {tasks.map((task) => (
